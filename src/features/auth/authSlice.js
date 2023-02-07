0500975db740a51cdc6eb483bd985663f1d8188d
Md Asaduzaman <asadasad9282@gmail.com>
2023-02-08 00:23:54 +0600

    authslice initial stracture

--- src/features/auth/authSlice.js
+++ src/features/auth/authSlice.js
@@ -1,4 +1,5 @@
-import { createSlice } from "@reduxjs/toolkit"
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
+import { createUserWithEmailAndPassword } from "firebase/auth"
 
 
 const initialState = {
@@ -9,11 +10,35 @@ const initialState = {
     error: '',
     isSuccess: false,
 }
+
+const crateuser = createAsyncThunk('authetication/create_user', async ({ auth, email, pass }) => {
+
+    const data = await createUserWithEmailAndPassword(auth, email, pass)
+    return data;
+})
+
 const authSlice = createSlice({
     name: "auth",
     initialState,
-    reducers: {
-
+    extraReducers: (builder) => {
+        builder
+            .addCase(crateuser.pending, (state) => {
+                state.isLoading = true;
+                state.isError = false;
+                state.error = ''
+            })
+            .addCase(crateuser.fulfilled, (state, action) => {
+                state.isLoading = false;
+                state.email = action.payload;
+                state.isError = false;
+                state.error = ''
+            })
+            .addCase(crateuser.rejected, (state, action) => {
+                state.isLoading = false;
+                state.email = '';
+                state.isError = true;
+                state.error = action.error.message
+            })
     }
 })
 
